01b0e7f987219e4fcbe4b10c7de8849797b16a0f
vaishnavisubramanian <vaishnavisubramanian11@gmail.com>
2022-04-29 17:02:22 +0530

    abstract class

---
+++ java/encap/src/com/Abstract/Animals.java
@@ -0,0 +1,43 @@
+package com.Abstract;
+
+public class Animals {
+   public static void main(String args[]){
+       Mouse obj1 = new Mouse();
+       obj1.eat();
+       obj1.sleep();
+       obj1.work();
+
+       Rabbit obj2 = new Rabbit();
+       obj2.all();
+
+   }
+}
+
+abstract class Animal{
+    abstract void eat();
+    abstract void sleep();
+}
+class Mouse {
+    void eat(){
+        System.out.println("Mouse eats cheese");
+    }
+    void sleep(){
+        System.out.println("Mouse sleeps for 4 hours");
+    }
+    void work(){
+        System.out.println("Mouse works for 1 hour");
+    }
+}
+class Rabbit{
+    void eat(){
+        System.out.println("Rabbit eats carrot");
+    }
+    void sleep(){
+        System.out.println("Rabbit sleeps for 12 hours");
+    }
+    void all(){
+        eat();
+        sleep();
+
+    }
+}
