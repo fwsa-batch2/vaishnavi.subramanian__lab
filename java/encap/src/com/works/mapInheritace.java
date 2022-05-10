1375700e25ad729a63d550103c32e046d80596f2
vaishnavisubramanian <vaishnavisubramanian11@gmail.com>
2022-05-10 12:13:28 +0530

    jsfc

---
+++ java/encap/src/com/works/mapInheritace.java
@@ -0,0 +1,13 @@
+package com.works;
+
+import java.util.HashMap;
+
+public class mapInheritace {
+    public static void main(String[] args) {
+        HashMap<Integer,String> names = new HashMap<Integer, String>();
+        names.put(1,"Vaishnavi");
+        names.put(3,"Hasan");
+        names.put(2,"Chithu");
+        System.out.println(names);
+    }
+}
